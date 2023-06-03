e43e9371ada7a0dd342152f9b4a12e6a4ea311b4
Hicham Zaidi <hzaidi@unibitsoft.com>
2023-06-03 12:15:57 +0100

    delete added

--- controllers/brandEngagementController.js
+++ controllers/brandEngagementController.js
@@ -1,13 +1,15 @@
 import { StatusCodes } from "http-status-codes";
 import BrandEngagement from "../model/BrandEngagement.js";
+import { badRequestError, notFoundError} from "../errors/index.js";
 
 const saveBrandEngagement = async (req, res) => {
   const { Timezone, CompanySector, BrandTone, TargetAudience,PostType,postContent } = req.body;
 
   if (!Timezone || !CompanySector || !BrandTone | !TargetAudience | !PostType | !postContent) {
-    throw new BadRequestError('Please provide all values')
+    throw new badRequestError('Please provide all values')
   }
   req.body.createdBy = req.user.userId
+  
   const brandEngagement = await BrandEngagement.create(req.body)
   res.status(StatusCodes.CREATED).json({ brandEngagement })
 };
@@ -28,6 +30,22 @@ const getBrandManagements = async (req, res, next) => {
   }
 };
 
+const deleteBrandEngagement = async (req, res) => {
+  const { id: brandId } = req.params
+
+  const brandEngagement = await BrandEngagement.findOne({ _id: brandId })
+
+  if (!brandEngagement) {
+    throw new notFoundError(`No brandEngagement with id :${brandId}`)
+  }
+
+  // checkPermissions(req.user, brandEngagement.createdBy)
+
+  await brandEngagement.remove()
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! brandEngagement removed' })
+}
 
 
-export { saveBrandEngagement,getBrandManagements };
+
+export { saveBrandEngagement,getBrandManagements, deleteBrandEngagement };
--- routes/brandEngagementRoutes.js
+++ routes/brandEngagementRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getBrandManagements, saveBrandEngagement } from "../controllers/brandEngagementController.js";
+import { deleteBrandEngagement, getBrandManagements, saveBrandEngagement } from "../controllers/brandEngagementController.js";
 const router = express.Router();
 import authenticateUser from "../middleware/auth.js";
 
 router.route("/save-brand-engagement").post(authenticateUser,saveBrandEngagement);
 router.route("/brand-engagements").get(authenticateUser,getBrandManagements);
+router.route("/brand-engagements/:id").delete(deleteBrandEngagement)
 
 export default router;
 
