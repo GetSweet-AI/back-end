24f818ad00dde7279612ea427a81fa526e0a57bb
Hicham Zaidi <hzaidi@unibitsoft.com>
2023-06-04 18:45:33 +0100

    jswt error fixed

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET,"1d");
+    const payload = jwt.verify(token, "y/B?E(H+MbPeShVmYq3t6w9z$C&F)J@N");
     console.log("Decoded Payload:", payload);
     req.user = { userId: payload.userId };
     console.log("User ID:", req.user.userId);
