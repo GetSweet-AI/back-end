0cc1fc09fe87a9501813251661553de6d3a444a9
Hicham Zaidi <hzaidi@unibitsoft.com>
2023-07-19 10:12:55 +0100

    getAllPostsFeed added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import authorize from "../middleware/authorize-role.js";
-import { getUsers, updateUserRole } from "../controllers/adminController.js";
+import { getFeedPostsForAdmin, getUsers, updateUserRole } from "../controllers/adminController.js";
 const router = express.Router();
 
 router.route("/users").get(getUsers);
-
 // Route to update user role
 router.put("/users/:userId/update-role", updateUserRole);
+router.get("/feedposts",getFeedPostsForAdmin)
 
 
 export default router;
